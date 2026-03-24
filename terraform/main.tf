1de8493bd9ed27ef8e223737bd479c1f51613a68
Anthony Barrett <abarrett_mbcs@outlook.com>
2026-03-24 16:00:30 +0000

    Add k3d cluster configuration in main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    k3d = {
+      source  = "pvgn/k3d"
+      version = "0.0.15" # Check for the latest version!
+    }
+  }
+}
+
+provider "k3d" {}
+
+resource "k3d_cluster" "phoenix" {
+  name    = "phoenix-cluster"
+  servers = 1
+  agents  = 2
+
+  # This ensures your 2011 MacBook can talk to the cluster locally
+  kube_api {
+    host_ip   = "127.0.0.1"
+    host_port = 6443
+  }
+
+  # Extra settings to make it "Ready for Work"
+  k3d {
+    disable_loadbalancer = false
+    disable_image_handler = false
+  }
+  
+  # This makes sure k3d uses a specific version of k3s
+  image = "rancher/k3s:v1.27.4-k3s1"
+}
